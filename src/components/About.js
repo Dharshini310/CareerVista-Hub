50f6b82bda468374fbb855e2f066b4ea021d6242
GUDDATI DHARSHINI <133137635+Dharshini310@users.noreply.github.com>
2025-04-02 10:11:26 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -22,12 +22,12 @@ export default class About extends Component {
 
         {/* Your other content goes here */}
         <img src="https://static.vecteezy.com/system/resources/previews/008/163/386/non_2x/coding-software-programming-concept-icon-illustration-for-computer-science-developer-poster-or-graphic-element-vector.jpg" alt="" width={300} height={300} id="g" />&nbsp;
-        <b id="x">Explore a variety of coding platforms that provide a comprehensive environment to enhance and refine your coding skills.&nbsp;<Link to="/blank" id="cu">CLICK HERE</Link></b><br />
+        <b id="x">Several coding platforms enhance skills through interactive learning and challenges.&nbsp;<Link to="/blank" id="cu">CLICK HERE</Link></b><br />
         
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTgUiJgWY6gPK4lEjy1T-z5SO_yQ0_DY9BHEk_W-coKIVV_mWFrGBPDgTzE6njkbf8BJ8&usqp=CAU" alt="" width={300} height={300} id="u" />&nbsp;
-        <b id="co">Discover a wide range of online courses that are tailored to help you gain in-depth knowledge and advance your expertise.&nbsp;<Link to="/course" id="cu">CLICK HERE</Link></b><br />
+        <b id="co">Several online course platforms enhance technical knowledge through structured learning.&nbsp;<Link to="/course" id="cu">CLICK HERE</Link></b><br />
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVg22xkpPuxYl3esBjZn3uyVzLdGD_FTL4lA&usqp=CAU" alt="" width={300} height={300} id="t" />&nbsp;       
-          <b id="in">Explore a variety of internship opportunities that allow you to apply your knowledge and skills in real-world settings.&nbsp;<Link to="/intern" id="cu">CLICK HERE</Link></b>
+          <b id="in">Several internship platforms allow you to apply your technical skills practically.&nbsp;<Link to="/intern" id="cu">CLICK HERE</Link></b>
             </div>
     )
   }
